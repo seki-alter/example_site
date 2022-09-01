9be024e6ef50c3e1389d2aec5d04a72e8a6d9286
seki-alter <sekigatsu.alter@gmail.com>
2022-09-01 11:08:59 +0200

    Add more views
    add views for: results, voting and viewing of questions

--- ex1/urls.py
+++ ex1/urls.py
@@ -4,5 +4,14 @@ from django.urls import re_path
 from . import views
 urlpatterns = [
     re_path(r'^$', views.index, name='index'),
-]
 
+    # ex: /ex1/5/
+    re_path(r'^(?P<question_id>\d+)/$', views.detail, name='detail'),
+
+    # ex: /ex1/5/results/
+    re_path(r'^(?P<question_id>\d+)/results/$', views.results, name='results'),
+
+    # ex: /ex1/5/vote/
+    re_path(r'^(?P<question_id>\d+)/vote/$', views.vote, name='vote'),
+
+]
--- ex1/views.py
+++ ex1/views.py
@@ -5,3 +5,13 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You are at the pools index.")
+
+def detail(request, question_id):
+    return HttpResponse("You are looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You are looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You are voting on question %s." % question_id)
